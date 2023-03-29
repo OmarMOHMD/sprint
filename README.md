2a453c454d641f6b0bffea9c8624e072ffb7b269
Omar Mohamed <omar.kungfu12@gmail.com>
2023-03-28 20:29:58 -0700

    commit all

--- README.md
+++ README.md
@@ -2,12 +2,43 @@
 
 ## Project setup
 ```
-npm install
+vue create sprint 
 ```
 
 ### Compiles and hot-reloads for development
 ```
 npm run serve
+```
+
+### Vuetify 
+```
+vue add vuetify
+```
+
+### Material-icons
+```
+npm i material-design-icons-iconfont -D
+```
+
+### Fortawesome
+```
+npm i @fortawesome/fontawesome-free -D
+```
+
+### Err on install echarts so you will need to enable this
+```
+npm config set legacy-peer-deps true
+```
+
+### echarts
+```
+npm i echarts vue-echarts
+```
+
+
+### Composition-api 
+```
+npm i -D @vue/composition-api
 ```
 
 ### Compiles and minifies for production
---
+++ src/components/SideBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-navigation-drawer v-model="drawer" app color="white" mini-variant mini-variant-width="80" >
+        <v-avatar class="d-block text-center mx-auto mt-4" size="40">
+            <v-icon color="#49D4A0">fab fa-atlassian</v-icon>
+        </v-avatar>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    data: () =>({
+        SelectItem: 0,
+        drawer: null,
+        items: [
+            {icon: 'fas fa-plus'},
+            { icon: 'fas fa-th-large' },
+            { icon: 'fas fa-align-center' },
+            { icon: 'fas fa-gitter' },
+            { icon: 'fas fa-chart-line' },
+        ],
+
+    })
+}
+</script>
+
+<style>
+    .border {
+        margin-left: 12px;
+        margin-right: 12px;
+        background: #49D9A0;
+        border-radius:50%;
+        text-decoration: none;
+    }
+    .v-list-item-group .v-list-item--active {
+        color: white !important;
+    }
+</style>
